e6d975292e556c4f81b0ee519436d1dd503b7802
kyngkyri <montique4ever@gmail.com>
2017-07-11 12:07:22 -0700

    first commit

---
+++ monti.js
@@ -0,0 +1,1 @@
+console.log("monti is awesome");
---
+++ pokemonVariable.js
@@ -0,0 +1,10 @@
+var charizardHP = 9000
+var charizardNum = 10
+var charizardType = "water and ice"
+var charizardBestMove = "splash"
+var charizardLastEvo = true
+var charizardAttack = 5
+var charizardDefense = 5000
+var charizardSpeed = 10000000
+var charizardSpecialAttack = 13
+var charizardSpecialDefense = 113
