1f3246d1e2bb5578a54e9e3ed5d1440aa1794aa8
kyngkyri <montique4ever@gmail.com>
2017-07-13 15:12:47 -0700

    added new files

--- pokemon-roster.js
+++ pokemon-roster.js
@@ -1,2 +1,17 @@
-var pokemonRoster = [["Bulbasaur", "grass/posion" 45, 49, 49, "not legend"],["Charmander", "fire", 39, 52, 43, "not legend"], ["Squirtle", "water", 44, 48, 65, "not legend"], ["Jigglypuff", "fairy/normal", 115, 45, 20, "not legend"], ["Raichu", "electric", 60, 90, 55, "legend"], ["Machamp", "fighting", 90, 130, 80, "legend"]]
-for 
+var pokemonRoster = [
+    ["Bulbasaur", "grass/posion", 45, 49, 49, false], //row 0
+    ["Charmander", "fire", 39, 52, 43, false], //row 1
+    ["Squirtle", "water", 44, 48, 65, false], //row 2
+    ["Jigglypuff", "fairy/normal", 115, 45, 20, false], //row 3
+    ["Raichu", "electric", 60, 90, 55, true], //row 4
+    ["Machamp", "fighting", 90, 130, 80, true] //row 5
+]
+for (var i=0; i < pokemonRoster.length; i++){
+    var line = ">Name: " + pokemonRoster[i][0];
+    line = line + " >Type: " + pokemonRoster[i][1];
+    line = line + " >HP: " + pokemonRoster[i][2];
+    line = line + " >Attack: " + pokemonRoster[i][3];
+    line = line + " >Defense: " + pokemonRoster[i][4];
+    line = line + " >Legendary: " + pokemonRoster[i][5];
+    console.log(line);
+}
