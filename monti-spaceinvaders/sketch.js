5b555321038f5870fd808ce2e0f4307f4c658531
kyngkyri <montique4ever@gmail.com>
2017-07-24 12:01:53 -0700

    added new files

--- monti-spaceinvaders/sketch.js
+++ monti-spaceinvaders/sketch.js
@@ -1,4 +1,4 @@
-var ship;
+/* var ship;
 var flowers = [];
 var drops = [];
 
@@ -95,9 +95,86 @@ function Drop(x, y) {
 
     
 }
+*/
+
+var ship;
+var flowers = [];
+var drops = [];
+
+function setup() {
+   createCanvas(windowWidth,windowHeight);
+   ship = new Ship();
+   //drop = new Drop(width/2,height/2);
+   for (var i = 0; i<16; i++) {
+       flowers[i]= new Flower(i*80+80, 60);
+   }
+}
+function draw() {
+   background(51);
+   ship.show();
+   for (var i = 0; i < drops.length; i++) {
+       drops[i].show();
+       drops[i].move();
+   }
+   for (var i = 0; i<flowers.length; i++) {
+       flowers[i].show();
+   }
+}
+
+function Ship() {
+   this.x = width/2;
 
 
+   this.show = function() {
+       fill(0,255,0);
+       rectMode(CENTER);
+       rect(this.x,height-20,20,60);
 
+   }
+
+   this.move = function(dir) {
+       this.x += dir*40;
+   }
+}
+
+function keyPressed() {
+   if (key === ' ') {
+       var drop = new Drop(ship.x, height)
+       drops.push(drop);
+   }
+   if (keyCode === RIGHT_ARROW) {
+       ship.move(5);
+   }else if (keyCode === LEFT_ARROW){
+       ship.move(-5);
+   }
+}
+
+function Flower(x, y){
+    this.x = x;
+    this.y = y;
+
+   this.show = function() {
+       fill(255,255,0);
+       ellipse(this.x,this.y,60,60);
+
+   }
+}
+
+function Drop(x, y) {
+   this.x = x;
+   this.y = y;
+
+   this.show = function() {
+       noStroke();
+       fill(0,0,255)
+       ellipse(this.x,this.y,16,18)
+   }
+
+   this.move = function() {
+       this.y = this.y - 5;
+   }
+
+}
 
 
 
